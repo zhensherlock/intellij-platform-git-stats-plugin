b2d847d850a6258aec4a26bd10405442452e817f
MichaelSun <zhensherlock@126.com>
2026-06-07 13:44:35 +0800

    build: add conventional commit pre-commit hooks

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,18 @@ pluginManagement {
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
     id("org.jetbrains.intellij.platform.settings") version "2.16.0"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.1.18"
+}
+
+gitHooks {
+    commitMsg {
+        conventionalCommits {
+            defaultTypes()
+            types("release")
+        }
+    }
+
+    createHooks(true)
 }
 
 @Suppress("UnstableApiUsage")
